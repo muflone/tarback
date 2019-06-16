c9770240ea0d9567a3f445ebd859798742b64e03
Muflone <webreg@vbsimple.net>
2019-06-16 20:49:33 +0200

    Added function prepare_tar_cmdline

--- tarback.py
+++ tarback.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 import argparse
 import ConfigParser
+import datetime
+import glob
+import os.path
 
 
 def parse_arguments():
@@ -37,10 +40,79 @@ def get_parser_option(parser, data_type, section, option, default):
     return result
 
 
+def prepare_tar_cmdline(parser):
+    directory = get_parser_option(parser, str, 'general', 'target', '.')
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
+
+    arguments = ['tar', ]
+    arguments.append('--create')
+    arguments.append('--preserve-permissions')
+    arguments.append('--exclude-backups')
+    arguments.append('--recursion')
+    arguments.append('--no-check-device')
+    arguments.append('--totals')
+    arguments.append('--listed-incremental={FILENAME}'.format(
+        FILENAME=os.path.join(directory, 'backup.snar')))
+    if get_parser_option(parser, int, 'general', 'followlinks', 1) == 1:
+        arguments.append('--dereference')
+
+    value = get_parser_option(parser, str, 'general', 'format', '')
+    if value == 'gzip':
+        arguments.append('--gzip')
+        output_extension = 'tar.gz'
+    elif value == 'bzip2':
+        arguments.append('--bzip2')
+        output_extension = 'tar.bz2'
+    elif value == 'xz':
+        arguments.append('--xz')
+        output_extension = 'tar.xz'
+    elif value == 'lzip':
+        arguments.append('--lzip')
+        output_extension = 'tar.lzip'
+    elif value == 'lzop':
+        arguments.append('--lzop')
+        output_extension = 'tar.lzop'
+    elif value == 'lzma':
+        arguments.append('--lzma')
+        output_extension = 'tar.lzma'
+    elif value == 'zstd':
+        arguments.append('--zstd')
+        output_extension = 'tar.zstd'
+    else:
+        output_extension = 'tar'
+
+    # Loads the snapshots list
+    filename_length = (len('backup_') +
+                       len(current_time) +
+                       1 +
+                       len(output_extension))
+    snapshots_list = []
+    for filename in sorted(os.listdir(directory)):
+        if (len(filename) == filename_length and
+                filename.startswith('backup_') and
+                filename.endswith('.' + output_extension)):
+            print(filename)
+            snapshots_list.append(filename)
+    # Determine snapshot filename
+    output_filename = 'backup_{DATETIME}.{EXTENSION}'.format(
+        DATETIME=current_time, EXTENSION=output_extension)
+    arguments.append('--file={FILENAME}'.format(
+        FILENAME=os.path.join(directory, output_filename)))
+
+    if parser.has_section('dirconfig'):
+        for option in parser.options('dirconfig'):
+            value = get_parser_option(parser, int, 'dirconfig', option, 1)
+            if value == 1:
+                arguments.append('--add-file={PATH}'.format(PATH=option))
+            else:
+                arguments.append('--exclude={PATH}'.format(PATH=option))
+
+
+    return arguments
+
+
 if __name__ == '__main__':
     args = parse_arguments()
     parser = parse_configuration_file(args.configuration)
-    for section in parser.sections():
-        print(section)
-        for option in parser.options(section):
-            print('  ', option, parser.get(section, option))
+    cmdline = prepare_tar_cmdline(parser)
+    print(' '.join(cmdline))
