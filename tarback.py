3a3d79f2f0515ac9e59d89c51d1af87e3fbd0b85
Muflone <webreg@vbsimple.net>
2019-06-16 20:42:49 +0200

    Added command line parsing

--- tarback.py
+++ tarback.py
@@ -1,10 +1,30 @@
+from __future__ import print_function
 import argparse
+import ConfigParser
 
-if __name__ == '__main__':
+
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('configuration', type=str,
+    parser.add_argument('configuration',
+                        type=str,
                         help='configuration file to use')
-    parser.add_argument('-v', '--verbose', action='store_true',
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
                         help='output verbose')
     args = parser.parse_args()
-    print(args)
+    return args
+
+
+def parse_configuration_file(configuration_filename):
+    parser = ConfigParser.RawConfigParser()
+    parser.read(configuration_filename)
+    return parser
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    parser = parse_configuration_file(args.configuration)
+    for section in parser.sections():
+        print(section)
+        for option in parser.options(section):
+            print('  ', option, parser.get(section, option))
