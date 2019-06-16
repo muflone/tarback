ab5879c6e870b2f48693a94fb37d6a93d8f396e9
Muflone <webreg@vbsimple.net>
2019-06-16 20:47:46 +0200

    Added get_parser_options

--- tarback.py
+++ tarback.py
@@ -22,6 +22,21 @@ def parse_configuration_file(configuration_filename):
     return parser
 
 
+def get_parser_option(parser, data_type, section, option, default):
+    result = default
+    if parser.has_section(section):
+        if parser.has_option(section, option):
+            if data_type is int:
+                result = parser.getint(section, option)
+            elif data_type is float:
+                result = parser.getfloat(section, option)
+            elif data_type is bool:
+                result = parser.getboolean(section, option)
+            else:
+                result = parser.get(section, option)
+    return result
+
+
 if __name__ == '__main__':
     args = parse_arguments()
     parser = parse_configuration_file(args.configuration)
